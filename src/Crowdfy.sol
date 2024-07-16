304ae98da43df404f569a72f4906577e2975ba3e
yebology <yobelnathaniel12@gmail.com>
2024-07-16 09:16:13 +0700

    first push

---
+++ src/Crowdfy.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Indetifier: MIT
+
+pragma solidity ^0.8.23;
+
+contract Crowdfy {
+    //
+
+    function createCampaign(
+        string memory campaignTitle,
+        string memory campaignDescription,
+        uint256 campaignEnd,
+        address campaignCreator
+    ) external {
+
+    }
+
+    function getCampaign() external view returns () {
+
+    }
+    //
+}
