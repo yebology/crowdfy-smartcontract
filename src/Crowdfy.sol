86fb21e40142b431dc02ae1f03915619ecc6a504
yebology <yobelnathaniel12@gmail.com>
2024-07-16 20:41:32 +0700

    done test

--- src/Crowdfy.sol
+++ src/Crowdfy.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.23;
 
+import {console} from "forge-std/console.sol";
+
 contract Crowdfy {
     //
     enum CampaignStatus {
@@ -31,7 +33,9 @@ contract Crowdfy {
 
     mapping(uint256 => Participant[]) private campaignParticipants;
 
-    event NewCampaignHasBeenCreated(uint256 indexed campaignId);
+    event NewCampaignHasBeenCreated(
+        uint256 indexed campaignId
+    );
     event CampaignStatusChanged(
         uint256 indexed campaignId,
         CampaignStatus indexed status
@@ -45,13 +49,18 @@ contract Crowdfy {
         uint256 indexed amount,
         bytes indexed data
     );
-    event ReceiveCalled(address indexed sender, uint256 indexed amount);
+    event ReceiveCalled(
+        address indexed sender, 
+        uint256 indexed amount
+    );
 
-    error CampaignIsClosed();
-    error InvalidTime();
-    error AmountMustBeGreaterThanZero();
-    error CampaignNotFound();
-    error TransferFailed(uint256 campaignId, uint256 amount);
+    error CampaignIsClosed(uint256 campaignId); // done test
+    error EmptyFieldExist(); // done test
+    error InvalidTime(uint256 campaignStart, uint256 campaignEnd); // done test
+    error AmountMustBeGreaterThanZero(uint256 amount); // done test
+    error InsufficientBalance(); // done test
+    error CampaignNotFound(uint256 searchId); // done test
+    error TransferFailed(uint256 campaignId, uint256 amount); // done test
 
     modifier checkTime(uint256 _campaignStart, uint256 _campaignEnd) {
         if (
@@ -59,14 +68,14 @@ contract Crowdfy {
             _campaignEnd < block.timestamp ||
             _campaignStart > _campaignEnd
         ) {
-            revert InvalidTime();
+            revert InvalidTime(_campaignStart, _campaignEnd);
         }
         _;
     }
 
     modifier checkStatus(uint256 _campaignId) {
         if (campaigns[_campaignId].status == CampaignStatus.CLOSED) {
-            revert CampaignIsClosed();
+            revert CampaignIsClosed(_campaignId);
         }
         _;
     }
@@ -81,26 +90,62 @@ contract Crowdfy {
             }
         }
         if (!isFound) {
-            revert CampaignNotFound();
+            revert CampaignNotFound(_campaignId);
         }
         _;
     }
 
-    modifier checkFunds(uint256 value) {
-        if (value <= 0) {
-            revert AmountMustBeGreaterThanZero();
+    modifier checkUserBalance(uint256 _userBalance, uint256 _amount) {
+        if (_userBalance < _amount) {
+            revert InsufficientBalance();
         }
         _;
     }
 
+    modifier checkFunds(uint256 _value) {
+        if (_value == 0) {
+            revert AmountMustBeGreaterThanZero(_value);
+        }
+        _;
+    }
+
+    modifier checkEmptyField(
+        string memory _campaignTitle,
+        string memory _campaignDescription,
+        uint256 _campaignStart,
+        uint256 _campaignEnd,
+        uint256 _fundsRequired
+    ) {
+        if (
+            bytes(_campaignTitle).length == 0 ||
+            bytes(_campaignDescription).length == 0 ||
+            _campaignStart == 0 ||
+            _campaignEnd == 0 ||
+            _fundsRequired == 0
+        ) {
+            revert EmptyFieldExist();
+        }
+        _;
+    }
+
+    // done test
     function createCampaign(
         string memory _campaignTitle,
         string memory _campaignDescription,
         uint256 _campaignStart,
         uint256 _campaignEnd,
-        uint256 _fundsRequired,
-        uint256 _currentRaised
-    ) external checkTime(_campaignStart, _campaignEnd) {
+        uint256 _fundsRequired
+    )
+        external
+        checkEmptyField(
+            _campaignTitle,
+            _campaignDescription,
+            _campaignStart,
+            _campaignEnd,
+            _fundsRequired
+        )
+        checkTime(_campaignStart, _campaignEnd)
+    {
         campaigns.push(
             Campaign({
                 id: campaigns.length,
@@ -110,14 +155,16 @@ contract Crowdfy {
                 campaignEnd: _campaignEnd,
                 campaignCreator: msg.sender,
                 fundsRequired: _fundsRequired,
-                currentRaised: _currentRaised,
+                currentRaised: 0 ether,
                 status: CampaignStatus.CLOSED
             })
         );
+        checkAndChangeCampaignStatus();
         emit NewCampaignHasBeenCreated(campaigns.length - 1);
     }
 
-    function checkAndChangeCampaignStatus() external {
+    // done test
+    function checkAndChangeCampaignStatus() public {
         uint256 campaignTotal = campaigns.length;
         for (uint256 i = 0; i < campaignTotal; i++) {
             CampaignStatus currentStatus = campaigns[i].status;
@@ -142,6 +189,7 @@ contract Crowdfy {
         }
     }
 
+    // done test
     function participateCampaign(
         uint256 _campaignId
     )
@@ -149,6 +197,7 @@ contract Crowdfy {
         payable
         checkCampaign(_campaignId)
         checkStatus(_campaignId)
+        checkUserBalance(address(msg.sender).balance, msg.value)
         checkFunds(msg.value)
     {
         address payable recipient = payable(
@@ -164,6 +213,7 @@ contract Crowdfy {
         }
     }
 
+    // done test
     function _addParticipantToMapping(
         uint256 _campaignId,
         address _sender,
@@ -179,10 +229,12 @@ contract Crowdfy {
         );
     }
 
+    // done test
     function getCampaigns() external view returns (Campaign[] memory) {
         return campaigns;
     }
 
+    // done test
     function getParticipant(
         uint256 _campaignId
     ) external view returns (Participant[] memory) {
--- test/CrowdfyTest.t.sol
+++ test/CrowdfyTest.t.sol
@@ -11,7 +11,7 @@ contract CrowdfyTest is Test {
     CrowdfyDeploy crowdfyDeploy;
     Crowdfy crowdfy;
 
-    uint256 private constant SEND_ETH = 0.1 ether; 
+    uint256 private constant SEND_ETH = 0.1 ether;
     address private constant BOB = address(1);
     address private constant ALICE = address(2);
 
@@ -19,12 +19,11 @@ contract CrowdfyTest is Test {
         vm.startPrank(ALICE);
         vm.deal(ALICE, 2 ether);
         crowdfy.createCampaign(
-            "Cancer Donation",
+            "Cancer Campaign",
             "Lorem ipsum dolor sit amet",
             block.timestamp,
             block.timestamp + 5 minutes,
-            1 ether,
-            0 ether
+            1 ether
         );
         vm.stopPrank();
         _;
@@ -35,23 +34,142 @@ contract CrowdfyTest is Test {
         crowdfy = crowdfyDeploy.run();
     }
 
-    function testSuccessfullyGetCampaigns() public createCampaign() {
+    function testSuccessfullyGetCampaigns() public createCampaign {
         uint256 expectedCampaignTotal = 1;
         uint256 actualCampaignTotal = crowdfy.getCampaigns().length;
         assertEq(expectedCampaignTotal, actualCampaignTotal);
     }
 
-    function testSuccessfullyParticipateInCampaign() public createCampaign() {
-        uint256 expectedCampaignParticipant = 1;
+    function testSuccessfullyParticipateInCampaign()
+        public
+        payable
+        createCampaign
+    {
         vm.startPrank(BOB);
         vm.deal(BOB, 2 ether);
         crowdfy.checkAndChangeCampaignStatus();
-        crowdfy.participateCampaign{
-            value: SEND_ETH
-        }(0);
+        crowdfy.participateCampaign{value: SEND_ETH}(0);
         vm.stopPrank();
+        uint256 expectedCampaignParticipant = 1;
         uint256 actualCampaignParticipant = crowdfy.getParticipant(0).length;
+        uint256 expectedSenderBalance = 1.9 ether;
+        uint256 actualSenderBalance = address(BOB).balance;
+        uint256 expectedRecipientBalance = 2.1 ether;
+        uint256 actualRecipientBalance = address(ALICE).balance;
         assertEq(expectedCampaignParticipant, actualCampaignParticipant);
+        assertEq(expectedSenderBalance, actualSenderBalance);
+        assertEq(expectedRecipientBalance, actualRecipientBalance);
     }
+
+    function testSuccessfullyRevertIfCampaignIsClosed() public {
+        crowdfy.createCampaign(
+            "Smoke Campaign",
+            "Lorem ipsum dolor sit amet",
+            block.timestamp + 1 minutes,
+            block.timestamp + 6 minutes,
+            1 ether
+        );
+        vm.expectRevert(
+            abi.encodeWithSelector(Crowdfy.CampaignIsClosed.selector, 0)
+        );
+        crowdfy.participateCampaign(0);
+    }
+
+    function testSuccessfullyRevertIfInvalidTime() public {
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                Crowdfy.InvalidTime.selector,
+                (block.timestamp + 2 minutes),
+                (block.timestamp + 1 minutes)
+            )
+        );
+        crowdfy.createCampaign(
+            "Poverty Campaign",
+            "Lorem ipsum dolor sit amet",
+            block.timestamp + 2 minutes,
+            block.timestamp + 1 minutes,
+            1 ether
+        );
+    }
+
+    function testSuccessfullyRevertIfAmountNotGreaterThanZero() public {
+        crowdfy.createCampaign(
+            "Animal Campaign",
+            "Lorem ipsum dolor sit amet",
+            block.timestamp,
+            block.timestamp + 1 minutes,
+            1 ether
+        );
+        crowdfy.checkAndChangeCampaignStatus();
+        uint256 invalidSendAmount = 0 ether;
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                Crowdfy.AmountMustBeGreaterThanZero.selector,
+                invalidSendAmount
+            )
+        );
+        crowdfy.participateCampaign{value: invalidSendAmount}(0);
+    }
+
+    function testSuccessfullyRevertIfInsufficientBalance() public {
+        crowdfy.createCampaign(
+            "Business Campaign",
+            "Lorem ipsum dolor sit amet",
+            block.timestamp,
+            block.timestamp + 1 minutes,
+            1 ether
+        );
+        crowdfy.checkAndChangeCampaignStatus();
+        vm.deal(BOB, 0.1 ether);
+        vm.startPrank(BOB);
+        vm.expectRevert(Crowdfy.InsufficientBalance.selector);
+        crowdfy.participateCampaign{value: SEND_ETH}(0);
+        vm.stopPrank();
+    }
+
+    function testSuccessfullyRevertIfCampaignNotFound() public {
+        crowdfy.createCampaign(
+            "Vegan Campaign",
+            "Lorem ipsum dolor sit amet",
+            block.timestamp,
+            block.timestamp + 1 minutes,
+            1 ether
+        );
+        crowdfy.checkAndChangeCampaignStatus();
+        vm.expectRevert(
+            abi.encodeWithSelector(Crowdfy.CampaignNotFound.selector, 1)
+        );
+        crowdfy.participateCampaign{value: SEND_ETH}(1);
+    }
+
+    function testSuccessfullyRevertIfTransferFailed() public {
+        crowdfy.createCampaign(
+            "Business Campaign",
+            "Lorem ipsum dolor sit amet",
+            block.timestamp,
+            block.timestamp + 1 minutes,
+            1 ether
+        );
+        crowdfy.checkAndChangeCampaignStatus();
+        vm.startPrank(BOB);
+        vm.deal(BOB, 1 ether);
+        vm.expectRevert(
+            abi.encodeWithSelector(Crowdfy.TransferFailed.selector, 0, SEND_ETH)
+        );
+        crowdfy.participateCampaign{value: SEND_ETH}(0);
+        vm.stopPrank();
+    }
+
+    function testSuccessfullyRevertIfEmptyFieldExist() public {
+        vm.expectRevert(Crowdfy.EmptyFieldExist.selector);
+        crowdfy.createCampaign(
+            "",
+            "Lorem ipsum dolor sit amet",
+            0,
+            1,
+            1 ether
+        );
+    }
+
     //
 }
